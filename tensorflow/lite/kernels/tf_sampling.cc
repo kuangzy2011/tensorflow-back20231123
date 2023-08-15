45467fad32825f18c339881c8b6f6da9c15b6047
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-08-16 00:03:03 +0800

    Update tf_sampling.cc

--- tensorflow/lite/kernels/tf_sampling.cc
+++ tensorflow/lite/kernels/tf_sampling.cc
@@ -22,16 +22,16 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
 
   const TfLiteTensor* input_inp = tflite::GetInput(context, node, 0);
   TF_LITE_ENSURE(context, input_inp != nullptr);
-  const TfLiteTensor* input_npoint = tflite::GetInput(context, node, 1);
-  TF_LITE_ENSURE(context, input_npoint != nullptr);
+  //const TfLiteTensor* input_npoint = tflite::GetInput(context, node, 1);
+  //TF_LITE_ENSURE(context, input_npoint != nullptr);
   TfLiteTensor* output = tflite::GetOutput(context, node, 0);
   TF_LITE_ENSURE(context, output != nullptr);
 
   const float* data_inp = tflite::GetTensorData<float>(input_inp);
-  const float* data_npoint = tflite::GetTensorData<float>(input_npoint);
+  //const float* data_npoint = tflite::GetTensorData<float>(input_npoint);
   float* data_output = tflite::GetTensorData<float>(output);
   
-  TF_LITE_KERNEL_LOG(context, "data of npoint: [0] %f, [1] %f\n", data_npoint[0], data_npoint[1]);
+  TF_LITE_KERNEL_LOG(context, "data of inp: [0] %f, [1] %f\n", data_inp[0], data_inp[1]);
   TF_LITE_KERNEL_LOG(context, "datatype for farthestpointsample Prepare output: %s\n", TfLiteTypeGetName(output->type));
   //return kTfLiteOk;
   return kTfLiteError;
