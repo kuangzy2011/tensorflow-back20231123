8c901362391eac25dd788bcdad33ad23aceb7877
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-08-16 15:52:09 +0800

    Update tf_sampling.cc

--- tensorflow/lite/kernels/tf_sampling.cc
+++ tensorflow/lite/kernels/tf_sampling.cc
@@ -31,7 +31,7 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   const float* data_inp = tflite::GetTensorData<float>(input_inp);
   float* data_output = tflite::GetTensorData<float>(output);
   
-  TF_LITE_KERNEL_LOG(context, "data of inp: [0] %.6f, [1] %.6f, [2] %.6f, [3] %.6f, [4] %.6f, [5] %.6f\n", data_inp[0], data_inp[1], data_inp[2], data_inp[3], data_inp[4], data_inp[5]);
+  //TF_LITE_KERNEL_LOG(context, "data of inp: [0] %.6f, [1] %.6f, [2] %.6f, [3] %.6f, [4] %.6f, [5] %.6f\n", data_inp[0], data_inp[1], data_inp[2], data_inp[3], data_inp[4], data_inp[5]);
   //return kTfLiteOk;
   return kTfLiteError;
 }
