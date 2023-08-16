15f4e14a737a05576a06a8ca4603e7a7e5cd6ef3
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-08-16 11:10:43 +0800

    Update tf_sampling.cc

--- tensorflow/lite/kernels/tf_sampling.cc
+++ tensorflow/lite/kernels/tf_sampling.cc
@@ -15,24 +15,24 @@ constexpr int kOutputTensor = 0;
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   //farthest_point_sample(inp, npoint)， 2 inputs and 1 output
-  TF_LITE_KERNEL_LOG(context, "NumberInputs %d, NumberOutputs %d\n", tflite::NumInputs(node), tflite::NumOutputs(node));
-
-  TF_LITE_ENSURE_EQ(context, tflite::NumInputs(node), 1);
-  TF_LITE_ENSURE_EQ(context, tflite::NumOutputs(node), 1);
-
   const TfLiteTensor* input_inp = tflite::GetInput(context, node, 0);
   TF_LITE_ENSURE(context, input_inp != nullptr);
   //const TfLiteTensor* input_npoint = tflite::GetInput(context, node, 1);
   //TF_LITE_ENSURE(context, input_npoint != nullptr);
   TfLiteTensor* output = tflite::GetOutput(context, node, 0);
   TF_LITE_ENSURE(context, output != nullptr);
-  TF_LITE_KERNEL_LOG(context, "Number Elements %ld\n", NumElements(input_inp));
+
+  
+  TF_LITE_KERNEL_LOG(context, "NumberInputs %d, NumberOutputs %d, input number elements %ld, number dimensions %d\n", tflite::NumInputs(node), tflite::NumOutputs(node), NumElements(input_inp), NumDimensions(input_inp));
+
+  //TF_LITE_ENSURE_EQ(context, tflite::NumInputs(node), 1);
+  //TF_LITE_ENSURE_EQ(context, tflite::NumOutputs(node), 1);
 
   const float* data_inp = tflite::GetTensorData<float>(input_inp);
   //const float* data_npoint = tflite::GetTensorData<float>(input_npoint);
   float* data_output = tflite::GetTensorData<float>(output);
   
-  //TF_LITE_KERNEL_LOG(context, "data of inp: [0] %f, [1] %f\n", data_inp[0], data_inp[1]);
+  TF_LITE_KERNEL_LOG(context, "data of inp: [0] %d, [1] %d\n", data_inp[0], data_inp[1]);
   TF_LITE_KERNEL_LOG(context, "datatype for farthestpointsample Prepare output: %s\n", TfLiteTypeGetName(output->type));
   //return kTfLiteOk;
   return kTfLiteError;
