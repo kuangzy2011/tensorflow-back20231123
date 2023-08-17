5c24008a3abe8efd51e58bc03098c04a843b7564
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-08-17 13:20:55 +0800

    Update tf_grouping.cc

--- tensorflow/lite/kernels/tf_grouping.cc
+++ tensorflow/lite/kernels/tf_grouping.cc
@@ -15,6 +15,17 @@ constexpr int kOutputTensor = 0;
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   TF_LITE_KERNEL_LOG(context, "[debug][queryballpoint][Prepare] ------------------1\n");
+
+  const TfLiteTensor* input_inp = tflite::GetInput(context, node, 0);
+  TF_LITE_ENSURE(context, input_inp != nullptr);
+  TfLiteTensor* output = tflite::GetOutput(context, node, 0);
+  TF_LITE_ENSURE(context, output != nullptr);
+
+  
+  TF_LITE_KERNEL_LOG(context, "[debug][queryballpoint][Prepare] - NumberInputs %d, NumberOutputs %d, input number elements %ld, number dimensions %d (%d, %d, %d)\n", tflite::NumInputs(node), tflite::NumOutputs(node), NumElements(input_inp), 
+    NumDimensions(input_inp), input_inp->dims->data[0], input_inp->dims->data[1], input_inp->dims->data[2]);
+  TF_LITE_KERNEL_LOG(context, "[debug][queryballpoint][Prepare] - datatype input_inp: %s, output %s\n", TfLiteTypeGetName(input_inp->type), TfLiteTypeGetName(output->type));
+
   return kTfLiteOk;
 }
 
@@ -40,6 +51,17 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
 
 TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
   TF_LITE_KERNEL_LOG(context, "[debug][grouppoint][Eval] ------------------1\n");
+
+  const TfLiteTensor* input_inp = tflite::GetInput(context, node, 0);
+  TF_LITE_ENSURE(context, input_inp != nullptr);
+  TfLiteTensor* output = tflite::GetOutput(context, node, 0);
+  TF_LITE_ENSURE(context, output != nullptr);
+
+  
+  TF_LITE_KERNEL_LOG(context, "[debug][grouppoint][Prepare] - NumberInputs %d, NumberOutputs %d, input number elements %ld, number dimensions %d (%d, %d, %d)\n", tflite::NumInputs(node), tflite::NumOutputs(node), NumElements(input_inp), 
+    NumDimensions(input_inp), input_inp->dims->data[0], input_inp->dims->data[1], input_inp->dims->data[2]);
+  TF_LITE_KERNEL_LOG(context, "[debug][grouppoint][Prepare] - datatype input_inp: %s, output %s\n", TfLiteTypeGetName(input_inp->type), TfLiteTypeGetName(output->type));
+
   return kTfLiteOk;
 }
 
