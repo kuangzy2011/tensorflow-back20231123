5552c3a470ccedcaf57ac3f27e8fe56702726df6
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-08-17 09:50:32 +0800

    Update tf_sampling.cc

--- tensorflow/lite/kernels/tf_sampling.cc
+++ tensorflow/lite/kernels/tf_sampling.cc
@@ -26,7 +26,7 @@ float3 Read3DLandmarkXYZ(const float* data, int idx) {
 }
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
-  TF_LITE_KERNEL_LOG(context, "farthestpointsample Prepare ------------------\n");
+  TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] ------------------1\n");
   
   //farthest_point_sample(inp, npoint)， 2 inputs and 1 output
   const TfLiteTensor* input_inp = tflite::GetInput(context, node, 0);
@@ -35,13 +35,13 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   TF_LITE_ENSURE(context, output != nullptr);
 
   
-  TF_LITE_KERNEL_LOG(context, "NumberInputs %d, NumberOutputs %d, input number elements %ld, number dimensions %d\n", tflite::NumInputs(node), tflite::NumOutputs(node), NumElements(input_inp), NumDimensions(input_inp));
-  TF_LITE_KERNEL_LOG(context, "input_inp dimensions: [0] %d, [1] %d, [2] %d\n", input_inp->dims->data[0], input_inp->dims->data[1], input_inp->dims->data[2]);
+  TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] - NumberInputs %d, NumberOutputs %d, input number elements %ld, number dimensions %d\n", tflite::NumInputs(node), tflite::NumOutputs(node), NumElements(input_inp), NumDimensions(input_inp));
+  TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] - input_inp dimensions: [0] %d, [1] %d, [2] %d\n", input_inp->dims->data[0], input_inp->dims->data[1], input_inp->dims->data[2]);
 
   const RuntimeShape input_shape = GetTensorShape(input_inp);
-  TF_LITE_KERNEL_LOG(context, "input_inp DimensionsCount %d, dimensions: [0] %d, [1] %d, [2] %d\n", input_shape.DimensionsCount(), input_shape.Dims(0), input_shape.Dims(1), input_shape.Dims(2));
+  TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] - input_inp DimensionsCount %d, dimensions: [0] %d, [1] %d, [2] %d\n", input_shape.DimensionsCount(), input_shape.Dims(0), input_shape.Dims(1), input_shape.Dims(2));
 
-  TF_LITE_KERNEL_LOG(context, "datatype input_inp: %s, output %s\n", TfLiteTypeGetName(input_inp->type), TfLiteTypeGetName(output->type));
+  TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] - datatype input_inp: %s, output %s\n", TfLiteTypeGetName(input_inp->type), TfLiteTypeGetName(output->type));
 
   //TF_LITE_ENSURE_EQ(context, tflite::NumInputs(node), 1);
   //TF_LITE_ENSURE_EQ(context, tflite::NumOutputs(node), 1);
@@ -50,12 +50,13 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   float* data_output = tflite::GetTensorData<float>(output);
   
   //TF_LITE_KERNEL_LOG(context, "data of inp: [0] %.6f, [1] %.6f, [2] %.6f, [3] %.6f, [4] %.6f, [5] %.6f\n", data_inp[0], data_inp[1], data_inp[2], data_inp[3], data_inp[4], data_inp[5]);
-  TF_LITE_KERNEL_LOG(context, "data of inp: [0] %f\n", data_inp[0]);
+  //TF_LITE_KERNEL_LOG(context, "data of inp: [0] %f\n", data_inp[0]);
 
-  int i = 0;
-  for(i = 0; i < 3; i++) {
-    //const float3 point = Read3DLandmarkXYZ(data_inp, i);
-    //TF_LITE_KERNEL_LOG(context, "data of inp index %d: {x: %.6f, y: %.6f, z: %.6f}\n", i, point.x, point.y, point.z);
+  int num_dims = NumDimensions(input_inp);
+  //TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] - data of inp: [0] %f\n", data_inp[0]);
+  //TfLiteIntArray* output_size = TfLiteIntArrayCreate(num_dims);
+  for (int i=0; i < num_dims; ++i) {
+    TF_LITE_KERNEL_LOG(context, "[debug][farthestpointsample][Prepare] -  dims %d: %d\n", input_inp->dims->data[i]);
   }
   
   //return kTfLiteOk;
