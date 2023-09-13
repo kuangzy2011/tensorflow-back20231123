38cbb7f4225b81cbea5e71fe229f22c77984dd4c
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-09-13 17:33:25 +0800

    Update sampling_op.h

--- tensorflow/lite/kernels/shim/test_op/sampling_op.h
+++ tensorflow/lite/kernels/shim/test_op/sampling_op.h
@@ -27,7 +27,7 @@ limitations under the License.
 #include "tensorflow/lite/kernels/shim/op_kernel.h"
 #include "tensorflow/lite/kernels/shim/status_macros.h"
 //#include "tensorflow/lite/kernels/shim/tflite_tensor_view.h"
-//#include "tensorflow/lite/core/interpreter.h"
+#include "tensorflow/lite/interpreter.h"
 
 namespace tflite {
 namespace shim {
@@ -141,7 +141,7 @@ Outputs
     auto output_ptr = output_t->template As<int32_t, 2>();
     auto out = output_t->template Data<int32_t>().data();
 
-    /*
+    
     ::tflite::Interpreter interpreter;
     interpreter.AddTensors(1);
     interpreter.AllocateTensors();
@@ -155,12 +155,12 @@ Outputs
     auto& t = t_or.value();
     
     auto temp = t.Data<float_t>();
-    */
-
+    
+    /*
     TfLiteTensor tflite_tensor;
     auto t = TensorView::New(&tflite_tensor);
     auto temp = t.Data<int32>();
-
+    */
 
     return absl::OkStatus();
   }
